1fc2c152010691871afebdb3336ca8762c127e0e
Jaewon Kim <jaewon.j.kim@snu.ac.kr>
2025-03-05 23:39:19 +0900

    gitignore update

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,7 @@
-CUDA_VISIBLE_DEVICES=3 python script_testing.py \
-    --model "LIGHTFUSE_sigmoid" \
-    --run_name 'kalan_tm_in_label' \
-    --epoch 20 \
-    --test_data './dataset_kalan_test' \
-    --input_tonemap 'mu' \
-    --label_tonemap 'mu' \
+CUDA_VISIBLE_DEVICES=0 python script_testing.py \
+    --model 'LIGHTFUSE_add_last_depthwise' \
+    --run_name 'sice' \
+    --epoch 10 \
+    --test_data './dataset_sice_valid' \
     --nChannel 6 \
     --nFeat 32
--- run_train.sh
+++ run_train.sh
@@ -1,12 +1,14 @@
-CUDA_VISIBLE_DEVICES=6,7 python script_training.py \
-    --model 'LIGHTFUSE_sigmoid' \
-    --run_name 'sice' \
-    --train_data './dataset_sice_train_patch' \
-    --valid_data './dataset_sice_valid' \
-    --nChannel 6 \
+CUDA_VISIBLE_DEVICES=0,1 python script_training.py \
+    --model 'LIGHTFUSE_3exp' \
+    --run_name 'kalan_f32' \
+    --train_data './dataset_kalan_train_patch_dior' \
+    --valid_data './dataset_kalan_test' \
+    --input_tonemap 'mu' \
+    --label_tonemap 'mu' \
+    --nChannel 9 \
     --nFeat 32 \
     --loss 'vgg' \
-    --epochs 10 \
+    --epochs 40 \
     --batchsize 128
 #CUDA_VISIBLE_DEVICES=6,7 python script_training.py \
 #    --model 'LIGHTFUSE_sigmoid' \
